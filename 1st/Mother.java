9c6cc961b608be5bb85a99d96413601705422176
241B047 <241B047@juet.co.in>
2026-08-10 15:10:47 +0530

    LE3 5codes

---
+++ 1st/Mother.java
@@ -0,0 +1,8 @@
+class Mother{
+    void show(){
+        System.out.println("mother");
+    }
+    void show(int x){
+        System.out.println("hello");
+    }
+}
---
+++ 2nd constructor/Mother (1).java
@@ -0,0 +1,8 @@
+class Mother{
+    Mother(int x){
+        System.out.println("Hello");
+    }
+    void show(){
+        System.out.println("Mother");
+    }
+}
